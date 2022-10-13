e99ea519cc8bb588d1946cb99cd534dd8c9b85bf
Lê Minh Thủy <lmthuy37@gmail.com>
2022-10-13 20:30:02 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -81,6 +81,7 @@ io.on('connection', async (socket)=>{
    socket.on("chooseConversation",async ({conversation,user_id}) =>{
       const gettAllMessages = await newMessage.find({conversation_id:conversation});
       const getConversation = await conversationUser.findOne({conversation_id:conversation, user_id:{"$ne":user_id}});
+      if(getConversation!=null)
       var getUser = await user.findOne({_id:getConversation.user_id});
       const userInfo = {
          fullname:getUser.fullname,
